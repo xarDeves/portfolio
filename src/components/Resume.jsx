c2fdfcc35efc20cf3f03c9d644cdcaf7b4666d5e
xarDeves <ch.nteves@gmail.com>
2023-08-20 18:10:50 +0300

    abjusted 'Resume' dimentions, got rid of eslint deactivation

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,27 +1,47 @@
-/* eslint-disable */
 import React, { useState, useEffect } from 'react';
 import endpoints from '../constants/endpoints';
+
+const styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+  },
+  pdfFrame: {
+    width: '100%',
+    height: '100%',
+    objectFit: 'contain',
+    border: 'none',
+    zoom: 3.5,
+  },
+};
 
 function Resume() {
   const [pdfPath, setPdfPath] = useState('');
 
   useEffect(() => {
     fetch(endpoints.resume)
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         console.log('Fetched PDF path:', data.pdfPath);
         setPdfPath(data.pdfPath);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error('Error fetching PDF path:', error);
       });
   }, []);
 
   return (
-    <div>
+    <div style={styles.container}>
       {pdfPath ? (
-        <iframe title="Resume" src={pdfPath} width="100%" height="100%" />
-        ) : (
+        <iframe
+          title="Resume"
+          src={pdfPath}
+          style={styles.pdfFrame}
+          allowFullScreen
+        />
+      ) : (
         <p>Loading...</p>
       )}
     </div>
